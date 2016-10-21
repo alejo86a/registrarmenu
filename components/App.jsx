31b4aeda62419ff42f05971b01d0e3ccf44a3507
Carlos <carloskikea@gmail.com>
2016-10-20 23:20:20 -0500

    add form in Component

--- components/App.jsx
+++ components/App.jsx
@@ -3,9 +3,30 @@ import React from 'react';
 class App extends React.Component {
    render() {
       return (
-         <div>
-            Hello World!!!
-         </div>
+      	 <div>
+	        <form>
+	          <div>
+	            <input type="email" placeholder="Name"/>
+	          </div>
+	          <div>
+	            <input type="text" placeholder="Price"/>
+	          </div>
+	          <div>
+	            <input type="text" placeholder="Descripcion"/>
+	          </div>
+	          <div>
+	          <select name="cars">
+				<option value="1">Restarante 1</option>
+				<option value="saab">Restarante 2</option>
+				<option value="mercedes">Restarante 3</option>
+				<option value="audi">Restarante 4</option>
+			  </select>
+	          </div>
+	          <div>
+	            <input type="submit/>
+	          </div>
+	        </form>
+	      </div>
       );
    }
 }
