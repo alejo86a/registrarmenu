de56e378996b3b3aa37304268139cc8d44afdc2c
Carlos <carloskikea@gmail.com>
2016-10-22 02:33:47 -0500

    Capture properties for components definition of params in post using function crearMenu

--- components/Form.jsx
+++ components/Form.jsx
@@ -7,31 +7,68 @@ var Form = React.createClass({
 
    mixins: [Reflux.connect(MenuStore, 'menustore')],
 
-   propTypes: {
-    value: React.PropTypes.object.isRequired,
+  //#region Definicion de propiedades
+  getInitialState () {
+    return {
+      name: React.PropTypes.string.isRequired,
+      price: React.PropTypes.string.isRequired,
+      description: React.PropTypes.string.isRequired,
+    };
   },
+  //#endregion
+
+  //#region Metodos
+  nameChange(e){    
+    this.setState({name: e.target.value })
+  },
+
+  priceChange(e){    
+    this.setState({price: e.target.value })
+  },
+
+  descriptionChange(e){    
+    this.setState({description: e.target.value })
+  },
+
+  changeHandler: function(e) {
+    this.setState({selected : e.target.value })
+  },
+
+  handleClick(e) {
+    if(name != "" && price != "" && description != "" && selected != 0){
+      MenuAction.crearMenu(name, price, description, selected); // React Component instance
+    }    
+  },
+  //#endregion 
 
   render: function() {
     return (
       React.createElement('form', {className: 'ContactForm'},
         React.createElement('input', {
           type: 'text',
-          placeholder: 'Name (required)',
-          //value: this.props.value.name,
-        }),
+          placeholder: 'Nombre (required)',          
+          onChange: this.nameChange.bind(this),
+        }, this.props.name),
         React.createElement('input', {
           type: 'text',
-          placeholder: 'Email',
-          //value: this.props.value.email,
-        }),
+          placeholder: 'Price (required)', 
+          onChange: this.priceChange.bind(this),         
+        }, this.props.price),
         React.createElement('input', {
           type: 'text',
-          placeholder: 'Description',
-          //value: this.props.value.description,
-        }),
+          placeholder: 'Descripción (required)',
+          onChange: this.descriptionChange.bind(this), 
+        }, this.props.price),
+        React.createElement('select', 
+          { value: this.state.selected, onChange: this.changeHandler},
+          React.createElement("option", { value: 0 }, "SELECCIONE UN RESTAURANTE"),
+          React.createElement("option", { value: 1 }, "RESTAURANTE 1"),
+          React.createElement("option", { value: 2 }, "RESTAURANTE 2"),
+          React.createElement("option", { value: 3 }, "RESTAURANTE 3"),
+        ),
         React.createElement('button', {
           type: 'submit',
-          onClick: MenuAction.crearMenu(),
+          onClick: this.handleClick(),
         }, "GUARDAR") ,
       )
     )
--- stores/MenuStore.js
+++ stores/MenuStore.js
@@ -6,15 +6,15 @@ var MenuStore = Reflux.createStore({
 
     listenables: [MenuActions],
 
-    crearMenu: function() {      
+    crearMenu: function(name, price, description, restaurant) {      
       $.ajax({
       	crossDomain: true,
       	cache: false,
       	context: this,
       	url: 'https://haskell-dojo.herokuapp.com/menus',
-  		method: 'POST',
-  		processData: false,
-  		data: '{\n\"restaurant\": 1,\n\"name\": \"Frijoles\",\n\"price\": 5000,\n\"description\": \"pollo\"\n}',
+  		  method: 'POST',
+  		  processData: false,
+  		  data: '{\n\"restaurant\": 1,\n\"name\": \name\",\n\"price\": price,\n\"description\": \description\"\n}',
 		success: function(data) {
           console.log(response);     
         }
